78239d4452cb71122404f59ef00aedd01e1a274e
HarshaReddy1602 <98198899+HarshaReddy1602@users.noreply.github.com>
2023-04-13 20:59:04 -0700

    Multiple IAM user creation and attaching policy to all users.

--- IAM/IAM_user_with_policies.tf
+++ IAM/IAM_user_with_policies.tf
@@ -1,3 +1,13 @@
+/*Hello All, There are multiple ways to create IAM users and attach policies using terraform. 
+		In this terraform project we are creating IAM USERS AND Attaching policies to the users, you can give any number of users you want to create in the variable file.
+
+
+IAM POLICIES, helps the user to access the AWS resources and for AWS it requires standard JSON format for creating policies.
+		
+You can specify each user different policy based on the user name in the aws_iam_user_policy resource 
+	*/
+
+
 provider "aws" {
   region = "us-east-2"
 }
@@ -28,11 +38,6 @@ policy = <<EOT
       "Effect": "Allow",
       "Resource": "*"
     }
-	
-	lifecyle{
-	create_before_destroy = true
-	}	
-
 
   ]
 }
